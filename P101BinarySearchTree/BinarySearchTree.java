ae5cfd9d108de6ddaaa4d622f74540dda8010a38
steve <pub@davisgroup.uk>
2026-09-02 20:13:32 -0500

    2

---
+++ P101BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,38 @@
+package P101BinarySearchTree;
+
+
+class BinarySearchTree<T extends Comparable<T>> implements SortedCollection<T> {
+
+    @Override
+    public void add(T data) throws NullPointerException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public boolean contains(Comparable<T> find) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clear'");
+    }
+
+}
+
+
