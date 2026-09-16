b28f1d9350ab1105c4a4a705c3639725c7edf476
steve <pub@davisgroup.uk>
2026-09-15 21:49:53 -0500

    1789526993

--- P102BSTRotation/BSTRotation.java
+++ P102BSTRotation/BSTRotation.java
@@ -73,7 +73,9 @@ public class BSTRotation<T extends Comparable<T>> extends BinarySearchTree<T> {
       child.setRight(parent);
       parent.setUp(child);
       parent.setLeft(b);
-      b.setUp(parent);
+      if (b != null) {
+        b.setUp(parent);
+      }
     }
     /*
      * Case 3: `child` is a right child of `parent`: left rotation
@@ -252,9 +254,40 @@ public class BSTRotation<T extends Comparable<T>> extends BinarySearchTree<T> {
     BSTRotation<String> stringTree = new BSTRotation<String>();
     stringTree.add("zoe");
     stringTree.add("roe");
-    // left rotation
+    // 0 shared children
     stringTree.rotate(stringTree.root.downLeft(), stringTree.root);
-    if (!(stringTree.root.getEntry() == "roe" && stringTree.root.downLeft().getEntry() == "zoe")) {
+    if (!(stringTree.root.getEntry() == "roe"
+        && stringTree.root.downLeft() == null
+        && stringTree.root.downRight().getEntry() == "zoe")) {
+      return false;
+    }
+    // 1 shared child
+    stringTree.add("yoe");
+    stringTree.rotate(stringTree.root.downRight(), stringTree.root);
+    if (!(stringTree.root.getEntry() == "zoe"
+        && stringTree.root.downLeft().getEntry() == "roe"
+        && stringTree.root.downLeft().downRight().getEntry() == "yoe")) {
+      return false;
+    }
+        // 2 shared children
+    stringTree.add("xoe");
+    stringTree.rotate(stringTree.root.downLeft(), stringTree.root);
+    if (!(stringTree.root.getEntry() == "roe"
+        && stringTree.root.downRight().getEntry() == "zoe"
+        && stringTree.root.downRight().downLeft().getEntry() == "yoe"
+        && stringTree.root.downRight().downLeft().downLeft().getEntry() == "xoe"
+      )) {
+      return false;
+    }
+         // 3 shared children
+    stringTree.add("voe");
+    stringTree.rotate(stringTree.root.downRight(), stringTree.root);
+    if (!(stringTree.root.getEntry() == "zoe"
+        && stringTree.root.downLeft().getEntry() == "roe"
+        && stringTree.root.downLeft().downRight().getEntry() == "yoe"
+        && stringTree.root.downLeft().downRight().downLeft().getEntry() == "xoe"
+        && stringTree.root.downLeft().downRight().downLeft().downLeft().getEntry() == "voe"
+      )) {
       return false;
     }
     return true;
